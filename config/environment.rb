4d5bacca995f0ca2c67b726b2393641d50c710fe
Matt Jaffe <matt.jaffe@yello.co>
2018-03-23 13:33:07 -0500

    Make rails s and rake db:setup work

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Footprints::Application.initialize!
-
-require File.expand_path('../../lib/patches/abstract_mysql_adapter', __FILE__)
