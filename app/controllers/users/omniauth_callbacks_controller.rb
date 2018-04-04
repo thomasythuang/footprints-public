6fac0b7f63582df87b6e54f492c6437fd35c0058
Thomas Huang <thomas.huang@yello.co>
2018-04-04 15:28:32 -0500

    move prefetchcraftsmen code into devise controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,11 @@
+require 'warehouse/prefetch_craftsmen'
+
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    prefetch_craftsmen
 
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
@@ -10,5 +14,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
+  end
+
+  def prefetch_craftsmen
+    auth_hash = request.env["omniauth.auth"]
+
+    Warehouse::PrefetchCraftsmen.new.execute(auth_hash[:extra][:id_token]) if Rails.application.config.prefetch_craftsmen
   end
 end
