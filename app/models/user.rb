14088eacf7847cf533bf1b51b6a0e9b9dd63a6ca
Matt Jaffe <matt.jaffe@yello.co>
2018-04-04 16:50:03 -0500

    Got google omniauth working but you guys should test it more

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,12 +30,12 @@ class User < ActiveRecord::Base
     user = User.where(email: data['email']).first
 
     # Uncomment the section below if you want users to be created if they don't exist
-    # unless user
-    #     user = User.create(name: data['name'],
-    #        email: data['email'],
-    #        password: Devise.friendly_token[0,20]
-    #     )
-    # end
+    unless user
+        user = User.create(
+           email: data['email'],
+           password: Devise.friendly_token[0,20]
+        )
+    end
     user
   end
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,33 +1,33 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  OmniAuth.config.logger = Rails.logger
-
-  if false
-    OmniAuth.config.test_mode = true
-
-    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
-      provider: "google_oauth2",
-      uid: "107478018817920458918",
-      info: {
-        name: "Megan Smith",
-        email: "you@abcinc.com",
-        first_name: "Megan",
-        last_name: "Smith"
-      },
-      extra: {
-        id_token: "ID_TOKEN"
-      }
-    })
-
-    provider :google_oauth2
-  else
-    begin
-      config      = Rails.root.join('config/omniauth.yml')
-      credentials = YAML.load_file(config)
-      provider :google_oauth2, credentials[Rails.env]["client_id"], credentials[Rails.env]["client_secret"]
-    rescue Exception => e
-      puts "OmniAuth configuration failed."
-      puts e
-      puts e.backtrace
-    end
-  end
+  # OmniAuth.config.logger = Rails.logger
+  #
+  # if false
+  #   OmniAuth.config.test_mode = true
+  #
+  #   OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+  #     provider: "google_oauth2",
+  #     uid: "107478018817920458918",
+  #     info: {
+  #       name: "Megan Smith",
+  #       email: "you@abcinc.com",
+  #       first_name: "Megan",
+  #       last_name: "Smith"
+  #     },
+  #     extra: {
+  #       id_token: "ID_TOKEN"
+  #     }
+  #   })
+  #
+  #   provider :google_oauth2
+  # else
+  #   begin
+  #     config      = Rails.root.join('config/omniauth.yml')
+  #     credentials = YAML.load_file(config)
+  #     provider :google_oauth2, credentials[Rails.env]["client_id"], credentials[Rails.env]["client_secret"]
+  #   rescue Exception => e
+  #     puts "OmniAuth configuration failed."
+  #     puts e
+  #     puts e.backtrace
+  #   end
+  # end
 end
