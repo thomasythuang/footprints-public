28f48103aab4da5e652b860debbb10de8d54f41b
Thomas Huang <thomas.huang@yello.co>
2018-06-05 21:33:51 -0500

    skip challenges made only by the logged in user

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -1,7 +1,9 @@
 class FightersController < ApplicationController
 
   def index
-    already_viewed_users = Challenge.pluck(:target_user_id).uniq
+    already_viewed_users = Challenge.where(user_id: current_user.id)
+                                    .pluck(:target_user_id)
+                                    .uniq
 
     @fighter = User.where.not(id: already_viewed_users + [current_user.id]).sample
   end
