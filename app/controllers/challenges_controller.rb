fea3cccde5c783bba2a002ff228a6cb3ebcfaea3
Thomas Huang <thomas.huang@yello.co>
2018-07-09 19:59:49 -0500

    only create a match when a user is challenged, not passed on

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -4,16 +4,22 @@ class ChallengesController < ApplicationController
     challenge = Challenge.new(
       user_id:        current_user.id,
       target_user_id: params[:target_user],
-      challenged:     params[:challenged]
+      challenged:     challenged?
     )
     challenge.save!
 
-    create_match if challenge.reverse_challenge.present?
+    create_match if challenged? && challenge.reverse_challenge.present?
 
     redirect_to :back
   end
 
   def create_match
     Match.create!(fighter_1_id: current_user.id, fighter_2_id: params[:target_user])
+  end
+
+  private
+
+  def challenged?
+    params[:challenged] == 'true'
   end
 end
